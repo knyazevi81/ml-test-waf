73c02b2e4905ecbbc52acd37c42260b25d6d4e7d
kornkawin <k.bijayayothin@gmail.com>
2024-12-01 21:09:31 +0700

    [FIREWALL] fixed bugs when classify query parameter

--- Testing/simple_testing.py
+++ Testing/simple_testing.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 with open('testing_app.json', 'r') as f:
     reqs_app = json.load(f)
@@ -8,10 +9,18 @@ with open('testing_waf.json', 'r') as f:
     reqs_wh_waf = json.load(f)
 
 if __name__ == '__main__':
+    print('Testing the applications')
+    time.sleep(3)
     # test the APP
     for req in reqs_app:
-        requests.request(**req)
-    
+        print(req)
+        response = requests.request(**req)
+        print(response.text)
+    print()
+    print('Testing the WAF')
+    time.sleep(3)
     # test the WAF
     for req in reqs_wh_waf:
-        requests.request(**req)
+        print(req)
+        response = requests.request(**req)
+        print(response.text)
--- WebAppFirewall/Firewall.py
+++ WebAppFirewall/Firewall.py
@@ -1,13 +1,16 @@
+import requests
 import Logger
 import os
-from flask import Flask, request
+from flask_cors import CORS
+from flask import Flask, request, jsonify
 from Classifier import ThreatClassifier
 from Request.Request import Request, interesting_header_list
 
 app = Flask(__name__)
 CLF = ThreatClassifier()
-LOGGER = Logger.init_logging('XssApp', 'Victim')
-WAF_URL = os.getenv('WAF_URL') if os.getenv('WAF_URL') else 'http://127.0.0.1:8000/waf'
+LOGGER = Logger.init_logging('Firewall', 'WebAppFirewall')
+APP_URL = os.getenv('APP_URL') if os.getenv('APP_URL') else 'http://127.0.0.1:8001'
+CORS(app)
 
 
 def flask_request_to_model_input(req: request) -> Request:
@@ -19,6 +22,15 @@ def flask_request_to_model_input(req: request) -> Request:
     for key in request_body:
         body += f"{key}={request_body[key]}&"
     body = body[:-1]
+    
+    # get query parameters
+    query_parameters = req.args.to_dict()
+    # add query parameters to the request path
+    if len(query_parameters) != 0:
+        req.path += '?'
+        for key in query_parameters:
+            req.path += f"{key}={query_parameters[key]}&"
+        req.path = req.path[:-1]
     
     # clean http headers
     headers = dict()
@@ -40,28 +52,39 @@ def flask_request_to_model_input(req: request) -> Request:
     return model_input
 
 
-# NO FIREWALL
-@app.route('/api/v1/hello/<string:name>', methods=['GET', 'POST'])
-def hello(name):
-    LOGGER.info('Request: %s', request)
-    return 'Hello, ' + name + '!'
-
-
-# WITH FIREWALL
-@app.route('/api/v1/hello-wh-waf/<string:name>', methods=['GET', 'POST'])
+@app.route('/api/v1/hello-wh-waf/<name>', methods=['GET', 'POST'])
 def hello_wh_waf(name):
     LOGGER.info('Request: %s', request)
     req = flask_request_to_model_input(request)
     req = CLF.classify_request(req)
     
     if len(req.threats) != 0 and 'valid' not in req.threats:
+        # threat detected
         LOGGER.info('Threats detected: %s', req.threats)
         LOGGER.info('Request blocked')
         return 'Forbidden', 403
 
     # good request
-    # return the response
-    return 'Hello, ' + name + '!'
+    # return the response from the app
+    endpoint = f'{APP_URL}/api/v1/hello/{name}'
+    LOGGER.info('Request forwarded to the app: %s', endpoint)
+    method = request.method
+    if method == 'POST':
+        data = request.form.to_dict()
+        response = requests.post(endpoint,
+                                 data=data,
+                                 headers=request.headers,
+                                 cookies=request.cookies,
+                                 files=request.files,
+                                 timeout=10,
+                                 verify=False)
+    else:
+        response = requests.get(endpoint,
+                                headers=request.headers,
+                                cookies=request.cookies,
+                                timeout=10,
+                                verify=False)
+    return response.text
 
 
 if __name__ == '__main__':
